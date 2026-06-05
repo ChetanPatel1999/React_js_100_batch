a11498a5d282084b716738f8f590190f7765b680
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-06-05 12:07:24 +0530

    04/06

---
+++ 12_components_project/src/App.jsx
@@ -0,0 +1,28 @@
+import Button from "./components/Button"
+import Card from "./components/Card"
+import Header from "./components/Header"
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <div className="parent">
+
+        <Card   />
+        <Card   />
+        <Card   />
+        <Card   />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ 12_components_project/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from './Button'
+
+const Card = ( ) => {
+    return (
+        <div className='card'>
+            <img src="https://images.unsplash.com/photo-1688001247541-43bbd88f77b9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YWN0b3IlMjBpbWFnZXxlbnwwfHwwfHx8MA%3D%3D"></img>
+            <h1>ram sharma</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <Button />
+        </div>
+    )
+}
+
+export default Card
