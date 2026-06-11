697bd9a27951588d40839353d2b47221c6b22496
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-06-11 12:11:47 +0530

    08/06

---
+++ 18_react_tailwind_project/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen'>
+      <div className='flex border m-4 flex-row justify-center items-center h-1/2'>
+        {/* <h1 className='bg-red-500 text-3xl text-white font-bold text-right'  >hello students</h1> */}
+        <div className='mycl bg-amber-300 border-5 border-red-500 rounded-full h-50 w-50 '>
+
+        </div>
+        <div className='bg-amber-300 border-5 border-red-500 rounded-full h-50 w-50 '>
+
+        </div>
+        <h1 className='uppercase'>hello wolrd institue</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
