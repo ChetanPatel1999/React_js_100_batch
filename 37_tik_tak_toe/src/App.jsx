efb23b0ac2d0a0ac3f8dcd0f245f3e7039826ac7
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-07-15 12:59:00 +0530

    07/07

---
+++ 37_tik_tak_toe/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Game from './components/Game'
+
+const App = () => {
+  return (
+    <div className='bg-gray-800 h-screen flex justify-center items-center'>
+      <Game />
+    </div>
+  )
+}
+
+export default App
---
+++ 37_tik_tak_toe/src/components/Game.jsx
@@ -0,0 +1,102 @@
+import React, { useRef, useState } from 'react'
+import circle_icon from '../assets/circle.png'
+import cross_icon from '../assets/cross.png'
+const Game = () => {
+
+    const [data, setdata] = useState(["", "", "", "", "", "", "", "", ""])
+    const [count, setcount] = useState(0)
+    const [lock, setlock] = useState(false)
+    const myref = useRef();
+    const toggle = (e, num) => {
+        if (lock) {
+            console.log("i am run")
+            return 0;
+        }
+        if (count % 2 == 0) {
+            e.target.innerHTML = `<img className='w-35 h-35 object-cover' src=${circle_icon} />`
+            setcount(count + 1);
+            let mydata = [...data];
+            mydata[num] = "o";
+            setdata(mydata);
+            winner(mydata);
+        }
+        else {
+            e.target.innerHTML = `<img className='w-35 h-35 object-cover' src=${cross_icon} />`
+            setcount(count + 1);
+            let mydata = [...data];
+            mydata[num] = "x";
+            setdata(mydata);
+            winner(mydata);
+        }
+    }
+    function winner(data) {
+        console.log(data)
+        if (data[0] == data[1] && data[1] == data[2] && data[2] != "") {
+            console.log("call")
+            won(data[0])
+        }
+        else if (data[3] == data[4] && data[4] == data[5] && data[5] != "") {
+            won(data[3])
+        }
+        else if (data[6] == data[7] && data[7] == data[8] && data[8] != "") {
+            won(data[6])
+        }
+        else if (data[0] == data[3] && data[3] == data[6] && data[6] != "") {
+            won(data[0])
+        }
+        else if (data[1] == data[4] && data[4] == data[7] && data[7] != "") {
+            won(data[1])
+        }
+        else if (data[2] == data[5] && data[5] == data[8] && data[8] != "") {
+            won(data[2])
+        }
+        else if (data[0] == data[4] && data[4] == data[8] && data[8] != "") {
+            won(data[0])
+        }
+        else if (data[2] == data[4] && data[4] == data[6] && data[6] != "") {
+            won(data[2])
+        }
+    }
+    function won(win) {
+        setlock(true);
+        if (win == "x") {
+            myref.current.innerHTML = `congratulation<img class='img' src=${cross_icon} />is winner `
+        }
+        else {
+            myref.current.innerHTML = `congratulation <img class='img' src=${circle_icon} /> is winner `
+        }
+    }
+    function reset() {
+        myref.current.innerHTML = `Tic Tac Toe In
+                <span class='my-span'> React</span>`
+
+        
+    }
+    return (
+        <div className='flex flex-col justify-center items-center'>
+            <h1 ref={myref} className='text-white text-3xl mb-4 font-bold text-center'>Tic Tac Toe In
+                <span className='text-green-300' > React</span></h1>
+            <div className='flex flex-col gap-1' >
+                <div className='flex gap-1 justify-center items-center'>
+                    <div onClick={(e) => toggle(e, 0)} className='w-30 h-30 bg-gray-700 rounded flex justify-center items-center' ></div>
+                    <div onClick={(e) => toggle(e, 1)} className='w-30 h-30 bg-gray-700 rounded flex justify-center items-center' ></div>
+                    <div onClick={(e) => toggle(e, 2)} className='w-30 h-30 bg-gray-700 rounded flex justify-center items-center' ></div>
+                </div>
+                <div className='flex gap-1 justify-center items-center'>
+                    <div onClick={(e) => toggle(e, 3)} className='w-30 h-30 bg-gray-700 rounded flex justify-center items-center' ></div>
+                    <div onClick={(e) => toggle(e, 4)} className='w-30 h-30 bg-gray-700 rounded flex justify-center items-center' ></div>
+                    <div onClick={(e) => toggle(e, 5)} className='w-30 h-30 bg-gray-700 rounded flex justify-center items-center' ></div>
+                </div>
+                <div className='flex gap-1 justify-center items-center'>
+                    <div onClick={(e) => toggle(e, 6)} className='w-30 h-30 bg-gray-700 rounded flex justify-center items-center' ></div>
+                    <div onClick={(e) => toggle(e, 7)} className='w-30 h-30 bg-gray-700 rounded flex justify-center items-center' ></div>
+                    <div onClick={(e) => toggle(e, 8)} className='w-30 h-30 bg-gray-700 rounded flex justify-center items-center' ></div>
+                </div>
+            </div>
+            <button onClick={reset}
+                className='bg-gray-700 text-green-300 py-3 px-8 mt-4 font-medium   rounded-4xl'>Reset</button>
+        </div>
+    )
+}
+
+export default Game
